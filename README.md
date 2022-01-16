e6be25909534af08aa613f30f8b79f49f5ed0fff
Flux <>
2022-01-16 15:03:14 +0700

    init example

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
-# eks-scaling
+# TF-controller demo: A self-managed EKS
+
+This is a TF-controller example to demonstrate manually scaling of a EKS cluster.
+
+### Initial seed the imported `TFSTATE`
+
+The cluster was created by EKSctl. Then the `terraform import` command was used to partially extract the node group resource of the cluster. 
+The node group definition was store here as a Terrform file.
+
+```shell
+terraform init
+terraform import aws_eks_node_group.ng-cc5a1ac4 tf-controller:ng-cc5a1ac4
+terraform show -no-color >> main.tf
+
+# correct the nodegroup resource the main.tf file
+terraform plan -out tfplan
+```
+
+If there's no change, prepare the initial tfstate secret using the following step.
+```
+gzip terraform.tfstate
+
+NAME=tf-controller-ng-cc5a1ac4
+kubectl create secret \
+  generic tfstate-default-${NAME} \
+  --from-file=tfstate=terraform.tfstate.gz \
+  --dry-run=client -o=yaml \
+  yq e '.metadata.annotations["encoding"]="gzip"' - > tfstate-default-${NAME}.yaml
+  
+kubectl apply -f tfstate-default-${NAME}.yaml
+```
+
+### Prepare an extra IAM Policy
+
+Here's an extra IAM Policy, attached to a role, used by this example to demonstrate the IRSA support. Please note that the cluster name is `tf-controller`, so that the policy was scoped to only this cluster. Please change the account id and the cluster name to those of yours. 
+```json
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "VisualEditor0",
+            "Effect": "Allow",
+            "Action": "eks:*",
+            "Resource": [
+                "arn:aws:eks:ap-southeast-1:<account id>:cluster/tf-controller",
+                "arn:aws:eks:ap-southeast-1:<account id>:cluster/tf-controller/*",
+                "arn:aws:eks:ap-southeast-1:<account id>:cluster/tf-controller/*/*",
+                "arn:aws:eks:ap-southeast-1:<account id>:nodegroup/tf-controller/*/*"
+            ]
+        }
+    ]
+}
+```
+
+### Plan and Apply to manage itself
+
+The cluster is now self-managable in a sense that we can change the number of its size via this Git repository.
+TF-controller will plan and apply the change, and the number of the real nodes of this cluster will be adjusted accordingly.
---
+++ _artifacts/30-terraform/tf-controller-nodegroup.tf
@@ -0,0 +1,79 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+variable "account_id" { }
+variable "region" { }
+
+provider "aws" {
+  region = var.region
+}
+
+locals {
+  # cluster size
+  cluster_size    = 3
+
+  # don't touch it
+  cluster_name    = "tf-controller"
+  node_group_name = "ng-cc5a1ac4"
+}
+
+# aws_eks_node_group.ng-cc5a1ac4:
+resource "aws_eks_node_group" "ng-cc5a1ac4" {
+    cluster_name    = local.cluster_name
+    node_group_name = local.node_group_name
+
+    version         = "1.21"
+    scaling_config {
+        desired_size = local.cluster_size
+        max_size     = local.cluster_size
+        min_size     = local.cluster_size
+    }
+
+    ami_type        = "AL2_x86_64"
+    capacity_type   = "ON_DEMAND"
+    disk_size       = 0
+    instance_types  = [
+        "m5.large",
+    ]
+
+    labels          = {
+        "alpha.eksctl.io/cluster-name"   = local.cluster_name
+        "alpha.eksctl.io/nodegroup-name" = local.node_group_name
+    }
+    node_role_arn   = "arn:aws:iam::${var.account_id}:role/eksctl-tf-controller-nodegroup-ng-NodeInstanceRole-KTZC6GLD9NTW"
+    release_version = "1.21.5-20220112"
+    subnet_ids      = [
+        "subnet-02cd58b8a7095c3c7",
+        "subnet-070f5075410d2b01c",
+        "subnet-07fcebbc4469f85b4",
+    ]
+    tags            = {
+        "alpha.eksctl.io/cluster-name"                = local.cluster_name
+        "alpha.eksctl.io/eksctl-version"              = "0.79.0"
+        "alpha.eksctl.io/nodegroup-name"              = local.node_group_name
+        "alpha.eksctl.io/nodegroup-type"              = "managed"
+        "eksctl.cluster.k8s.io/v1alpha1/cluster-name" = local.cluster_name
+    }
+    tags_all        = {
+        "alpha.eksctl.io/cluster-name"                = local.cluster_name
+        "alpha.eksctl.io/eksctl-version"              = "0.79.0"
+        "alpha.eksctl.io/nodegroup-name"              = local.node_group_name
+        "alpha.eksctl.io/nodegroup-type"              = "managed"
+        "eksctl.cluster.k8s.io/v1alpha1/cluster-name" = local.cluster_name
+    }
+
+    launch_template {
+        name    = "eksctl-tf-controller-nodegroup-${local.node_group_name}"
+        version = "1"
+    }
+
+    update_config {
+        max_unavailable = 1
+    }
+}
