2d93baeb0839a84e0bcbfc5f1b124ddf49b6094d
Chanwit Kaewkasi <chanwit@gmail.com>
2022-01-19 12:46:31 +0000

    add org and workspace

--- _artifacts/40-terraform/s3-bucket.tf
+++ _artifacts/40-terraform/s3-bucket.tf
@@ -1,8 +1,17 @@
 terraform {
+  required_version = ">= 1.1.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 3.0"
+    }
+  }
+
+  cloud {
+    organization = "weaveworks"
+
+    workspaces {
+      name = "dev"
     }
   }
 }
